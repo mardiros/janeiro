85334ef453be402ab91ac0537ff2ec96b03cb364
Guillaume Gauvrit <guillaume@gauvr.it>
2016-12-01 21:55:04 +0100

    Fix functionnal tests

--- tests/test_echo_server.rs
+++ tests/test_echo_server.rs
@@ -1,30 +1,47 @@
 extern crate janeiro;
 
-mod echo_server;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 
+use std::str;
 use std::thread;
+use std::process::Command;
 use std::time::Duration;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::io::BufReader;
 
 
-#[test]
-fn test_echo_server() {
 
-    thread::spawn(|| {
-        echo_server::main();
-    });
+// The test is ignored by default because in case of regression
+// it will leave the echo_server child process alive.
+#[test]
+#[ignore]
+fn test_echo_server() {
+    env_logger::init().unwrap();
+    info!("Test echo server");
+
+    let mut child = Command::new("cargo")
+        .arg("run")
+        .arg("--example=echo_server")
+        .spawn().unwrap();
+
+    info!("echo server started in pid {:?}", child.id());
 
     // wait the server start before connect
     for _ in 0..30 {
+        info!("Wait for connection");
         match TcpStream::connect("127.0.0.1:8888") {
             Ok(_) => break,
-            Err(_) => thread::sleep_ms(40),
+            Err(_) => {
+                thread::sleep(Duration::from_millis(10))
+            },
         }
     }
+
+
     let stream = TcpStream::connect("127.0.0.1:8888").unwrap();
-    let _ = stream.set_read_timeout(Some(Duration::new(0, 50_000)));
     let mut reader = BufReader::new(stream);
 
     let mut line = String::new();
@@ -41,4 +58,7 @@ fn test_echo_server() {
     let _ = reader.read_line(&mut line).unwrap();
     assert_eq!("bye\n", line);
 
+    // todo tests that the stream has been closed by peer here
+
+    child.kill().unwrap();
 }
